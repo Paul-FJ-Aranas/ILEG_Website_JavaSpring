adb8b5d8e83bb4e0d6261bf042a9c4e7643f80a3
Paul Aranas <paul.aranas@ymail.com>
2016-12-13 20:23:27 -0800

    feat: Add code in UserDao class to query database

--- src/com/paularanas/ileg/web/dao/UserDao.java
+++ src/com/paularanas/ileg/web/dao/UserDao.java
@@ -1,11 +1,14 @@
 package com.paularanas.ileg.web.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component("userDao")
@@ -14,7 +17,19 @@ public class UserDao {
 	private JdbcTemplate jdbcData;
 
 	public List<User> getUsers() {
-		return null;
+		return jdbcData.query("select * from users", new RowMapper<User>() {
+
+			@Override
+			public User mapRow(ResultSet arg0, int arg1) throws SQLException {
+				User user = new User();
+				user.setId(arg0.getInt("id"));
+				user.setName(arg0.getString("name"));
+				user.setEmail(arg0.getString("email"));
+				return user;
+			}}
+				);
+		 
+		
 	}
 
 	@Autowired
